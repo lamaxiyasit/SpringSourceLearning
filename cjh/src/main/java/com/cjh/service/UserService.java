de88cafc65bdff2836305da4b9532552572259a2
程佳辉 <jiahui.cheng@bl.com>
2025-03-13 17:12:04 +0800

    feat(cjh): 添加新模块并配置数据库连接

---
+++ cjh/src/main/java/com/cjh/service/UserService.java
@@ -0,0 +1,24 @@
+package com.cjh.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @Auther: 程佳辉
+ * @Date: 2025/3/13 16:29
+ * @Description:
+ */
+@Service
+public class UserService {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Transactional
+	public void test(){
+		jdbcTemplate.execute("insert into user_info(user_name,password,create_time,update_time,create_by,update_by) values('1','1',NOW(),NOW(),'1','1')");
+		throw new RuntimeException();
+	}
+}
